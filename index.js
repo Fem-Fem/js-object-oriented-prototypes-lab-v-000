6489ae7a3ed8ed75e0b8c5847562f07d26bcba80
Obafemi Fadairo <ofpupp@princeton.edu>
2018-08-18 22:33:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,10 +17,9 @@ BoardMember.prototype.doCharity = function(){
   }
 
 BoardMember.prototype.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
+    return "You will see great things from Scuber."
+  }
 //
 //   this.sayHi = function(){
 //     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
 //   }
-// }
